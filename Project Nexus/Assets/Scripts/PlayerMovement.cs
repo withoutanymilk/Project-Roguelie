e1dad8173508e04887b7ec80895a355ed80da4cd
withoutanymilk <squicky825@gmail.com>
2022-09-08 13:13:27 -0700

    HUGE!!!! Assets Update
    The Following Update Has the Following
    
    -Animations
    -Enemies Assets
    -FX Assets
    -Map Floor Assets
    -Map Wall Assets
    -Player/Hero Assets
    -Prop Items Assets
    -Scenes (Main Menu will be added by the end of the week hopefully still working on it)
    -Scripts
    -UI Assets
    -Weapons Assets

---
+++ Project Nexus/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    public Rigidbody2D rb;
+
+    public Animator anim;
+
+    private Vector2 moveDirection;
+    private Vector2 lastMoveDirection;
+
+    void Update()
+    {
+        ProcessInputs();
+        Animate();
+    }
+    // Start is called before the first frame update
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    // Update is called once per frame
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        if ((moveX == 0 && moveY == 0) && moveDirection.x != 0 || moveDirection.y != 0)
+        {
+            lastMoveDirection = moveDirection;
+        }
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
+    void Animate()
+    {
+        anim.SetFloat("AnimMoveX", moveDirection.x);
+        anim.SetFloat("AnimMoveY", moveDirection.y);
+        anim.SetFloat("AnimMoveMagnitude", moveDirection.magnitude);
+        anim.SetFloat("AnimLastMoveX", lastMoveDirection.x);
+        anim.SetFloat("AnimLastMoveY", lastMoveDirection.y);
+    }
+
+}
