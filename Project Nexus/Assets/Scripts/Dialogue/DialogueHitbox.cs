85cc2abaf08f772b7fc165c2673a3af842491c78
NotTom31 <thomasmezzie@gmail.com>
2022-11-17 21:26:01 -0800

    Working on dialogue

---
+++ Project Nexus/Assets/Scripts/Dialogue/DialogueHitbox.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DialogueHitbox : MonoBehaviour
+{
+    public UnityEvent DisplayText;
+    public UnityEvent NextSentence;
+    public UnityEvent CloseText;
+    public UnityEvent InteractIconOn;
+    public UnityEvent InteractIconOff;
+    public Animator animator;
+    public GameObject DialogueBox;
+    public bool collected;
+
+    void Awake()
+    {
+        animator = DialogueBox.GetComponent<Animator>();
+    }
+
+    protected void OnTriggerEnter2D(Collider2D obj)
+    {
+        if (Input.GetKeyDown("e") && collected == true)
+        {
+            NextSentence.Invoke();
+        }
+        if (Input.GetKeyDown("e") && collected == false)
+        {
+            collected = true;
+            DisplayText.Invoke();
+        } 
+    InteractIconOn.Invoke();
+    }
+
+    private void OnTriggerStay2D(Collider2D obj)
+    {
+        if(animator.GetBool("IsOpen") == false)
+        {
+            collected = false;
+        }
+    }
+
+        private void OnTriggerExit2D(Collider2D obj)
+    {
+        if (obj.CompareTag("Player"))
+        {
+            CloseText.Invoke();
+            InteractIconOff.Invoke();
+            collected = false;
+        }
+    }
+}
+
+
+
+
---
+++ Project Nexus/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    public Text nameText;
+    public Text contText;
+    public Text dialogueText;
+    public UnityEvent SetCollected;
+
+    public Animator animator;
+
+    private Queue<string> sentences;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        animator.SetBool("IsOpen", true);
+        nameText.text = dialogue.name;
+        contText.text = dialogue.contButton;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    public void EndDialogue()
+    {
+        animator.SetBool("IsOpen", false);
+        SetCollected.Invoke();
+    }
+
+}
