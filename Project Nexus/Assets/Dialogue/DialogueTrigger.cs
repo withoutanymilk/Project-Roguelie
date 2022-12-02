76e5d1fefea65cf10aa7335237b2ca569365c058
NotTom31 <thomasmezzie@gmail.com>
2022-12-02 00:20:02 -0800

    added dialogue box
    only in level 3 at the moment

---
+++ Project Nexus/Assets/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+    public void TriggerEndDialogue()
+    {
+        FindObjectOfType<DialogueManager>().EndDialogue();
+    }
+
+    public void TriggerNextSentence()
+    {
+        FindObjectOfType<DialogueManager>().DisplayNextSentence();
+    }
+}
